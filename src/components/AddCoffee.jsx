f688a71f928a97ab5047e99231b458dacf952a33
Imran <webimran2021@gmail.com>
2023-10-16 18:34:10 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h2>This is add coffe</h2>
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2>This is update coffe</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
