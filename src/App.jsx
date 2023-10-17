a68a0a6c143de713c5c5ffdf5bc074d5b467771d
Imran <webimran2021@gmail.com>
2023-10-17 19:03:27 +0600

    added button on card

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ function App() {
   const coffees = useLoaderData();
 
   return (
-    <div>
-      <h1 className='text-6xl text-center text-purple-600'>Hot Cold Coffee: {coffees?.length}</h1>
-      {
-        coffees?.map(coffee=> <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
-      }
+    <div className='m-20'>
+      <h1 className='text-6xl text-center my-20 text-purple-600'>Hot Cold Coffee: {coffees?.length}</h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          coffees?.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+        }
+      </div>
     </div>
   )
 }
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -6,11 +6,21 @@ const CoffeeCard = ({ coffee }) => {
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <figure><img src={photo} alt="Movie" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">New movie is released!</h2>
-                <p>Click the button to watch on Jetflix app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
+            <div className="flex justify-between w-full pr-4">
+                <div>
+                    <h2 className="card-title">{name}</h2>
+                    <p>{quantity}</p>
+                    <p>{supplier}</p>
+                    <p>{taste}</p>
+                    <p>{category}</p>
+                    <p>{details}</p>
+                </div>
+                <div className="card-actions justify-center">
+                    <div className="btn-group btn-group-vertical space-y-4">
+                        <button className="btn">View</button>
+                        <button className="btn">Edit</button>
+                        <button className="btn">Delete</button>
+                    </div>
                 </div>
             </div>
         </div>
